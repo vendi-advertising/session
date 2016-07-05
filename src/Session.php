b191bc1a9b56dcb77f766dd70596edc8bf861756
Kamal Khan <shout@bhittani.com>
2016-07-05 23:36:33 +0500

    Draft initial release with unit tests.

---
+++ src/Session.php
@@ -0,0 +1,145 @@
+<?php
+
+/*
+ * This file is part of the laraport/session package.
+ *
+ * (c) 2016 Kamal Khan <shout@bhittani.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Laraport;
+
+use SessionHandlerInterface;
+use Illuminate\Session\Store;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Session\FileSessionHandler;
+use Illuminate\Session\DatabaseSessionHandler;
+use Symfony\Component\HttpFoundation\Session\Storage\Handler\NullSessionHandler;
+
+class Session extends Store
+{
+    protected $Handler;
+
+    protected $config;
+
+    public function __construct($argument = null)
+    {
+        $this->config = require __DIR__ . '/config.php';
+
+        if(is_array($argument))
+            return $this->setConfig($argument);
+
+        if($argument instanceof SessionHandlerInterface)
+            return $this->setSessionHandler($argument);
+    }
+
+    protected function dispatch()
+    {
+        $name = $this->getName();
+
+        if(isset($_COOKIE[$name]) && $sessionId = $_COOKIE[$name])
+        {
+            $this->setId($sessionId);
+        }
+        else if(!isset($_COOKIE[$name]))
+        {
+            setcookie(
+                $name,
+                $this->getId(),
+                time() + 60 * $this->config['lifetime'],
+                $this->config['path'],
+                $this->config['domain'],
+                $this->config['secure'],
+                $this->config['http_only']
+            );
+        }
+
+        parent::start();
+
+        register_shutdown_function([$this, 'save']);
+    }
+
+    protected function arrayHandler()
+    {
+        return new NullSessionHandler;
+    }
+
+    protected function fileHandler()
+    {
+        return new FileSessionHandler(new Filesystem, $this->config['files']);
+    }
+
+    protected function databaseHandler()
+    {
+        $Database = new Manager;
+        $config = $this->config['connection'];
+        $Database->addConnection($config);
+        $Connection = $Database->getConnection('default');
+        $Schema = $Connection->getSchemaBuilder();
+		if(!$Schema->hasTable($this->config['table']))
+		{
+			$Schema->create($this->config['table'], function($Table)
+			{
+				$Table->string('id')->unique();
+				$Table->text('payload');
+				$Table->integer('last_activity');
+			});
+		}
+        return new DatabaseSessionHandler($Connection, $this->config['table']);
+    }
+
+    public function setConfig(array $config)
+    {
+        $this->config = array_replace($this->config, $config);
+        return $this;
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    public function setSessionHandler(SessionHandlerInterface $Handler)
+    {
+        $this->Handler = $Handler;
+        return $this;
+    }
+
+    public function getSessionHandler()
+    {
+        return $this->Handler;
+    }
+
+    public function start()
+    {
+        $name = $this->config['cookie'];
+        $lifetime = $this->config['lifetime'];
+        $sessionId = $this->generateSessionId();
+
+        if($this->config['driver'] instanceof SessionHandlerInterface)
+        {
+            $this->setSessionHandler($this->config['driver']);
+            parent::__construct($name, $this->Handler);
+            return $this->dispatch();
+        }
+
+        switch($this->config['driver'])
+        {
+            case 'database':
+                parent::__construct($name, $this->databaseHandler());
+            break;
+            case 'file':
+                parent::__construct($name, $this->fileHandler());
+            break;
+            default:
+            case 'array':
+                parent::__construct($name, $this->arrayHandler());
+            break;
+        }
+
+        return $this->dispatch();
+    }
+}
---
+++ test/SessionTest.php
@@ -0,0 +1,137 @@
+<?php
+
+/*
+ * This file is part of the laraport/session package.
+ *
+ * (c) 2016 Kamal Khan <shout@bhittani.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Laraport\Session;
+use Illuminate\Database\Capsule\Manager;
+use Symfony\Component\HttpFoundation\Session\Storage\Handler\NullSessionHandler;
+
+class SessionTest extends PHPUnit_Framework_TestCase
+{
+    protected $defaultConfig;
+
+    public function setUp()
+    {
+        $this->defaultConfig = require __DIR__ . '/../src/config.php';
+    }
+
+    public function assertions($Session)
+    {
+        $Session->put('hello', 'world');
+        $this->assertEquals('world', $Session->get('hello'));
+        $Session->flash('lights', 'green');
+        $this->assertEquals('green', $Session->get('lights'));
+        $Session->flush();
+        $this->assertEquals([], $Session->all());
+        $Session->put('hello', 'again');
+        $this->assertEquals('again', $Session->get('hello'));
+    }
+
+    /** @test */
+    public function it_should_init_with_default_configuration()
+    {
+        $Session = new Session;
+        $this->assertEquals($this->defaultConfig, $Session->getConfig());
+    }
+
+    /** @test */
+    public function it_should_allow_construction_with_config()
+    {
+        $config = [
+            'driver' => 'database',
+            'foo' => 'bar'
+        ];
+        $Session = new Session($config);
+        $assertion = array_replace($this->defaultConfig, $config);
+        $this->assertEquals($assertion, $Session->getConfig());
+    }
+
+    /** @test */
+    public function it_should_allow_construction_with_custom_handler()
+    {
+        $Session = new Session(new NullSessionHandler);
+        $this->assertTrue(
+            $Session->getSessionHandler() instanceof SessionHandlerInterface
+        );
+    }
+
+    /** @test */
+    public function it_should_default_to_array_session_handler()
+    {
+        $Session = new Session;
+        $Session->start();
+        $this->assertions($Session);
+    }
+
+    /** @test */
+    public function it_should_support_the_file_session_handler()
+    {
+        $fileSessionPath = __DIR__;
+
+        $Session = new Session([
+            'driver' => 'file',
+            'files' => $fileSessionPath
+        ]);
+
+        $Session->start();
+        $this->assertions($Session);
+
+        $filename = $Session->getId();
+        $this->assertEquals(40, strlen($filename));
+
+        $filepath = $fileSessionPath . '/' . $filename;
+
+        register_shutdown_function(function() use($filepath){
+            $this->assertFileExists($filepath);
+            unlink($filepath);
+        });
+    }
+
+    /** @test */
+    public function it_should_support_the_database_session_handler()
+    {
+        $database = __DIR__ . '/session.sqlite';
+        if(file_exists($database)) unlink($database);
+        touch($database);
+
+        $connection = [
+            'driver'   => 'sqlite',
+            'database' => $database,
+            'prefix'   => ''
+        ];
+
+        $table = 'sessions';
+
+        $Session = new Session([
+            'driver' => 'database',
+            'connection' => $connection,
+            'table' => $table
+        ]);
+
+        $Session->start();
+        $this->assertions($Session);
+
+        $sid = $Session->getId();
+
+        register_shutdown_function(function() use($sid, $connection, $table){
+            $Database = new Manager;
+            $Database->addConnection($connection);
+
+            $Db = $Database->getDatabaseManager();
+            $results = $Db->select('select id from ' . $table . ' LIMIT 1');
+
+            $this->assertEquals(1, count($results));
+            $row = array_shift($results);
+            $this->assertEquals($sid, $row['id']);
+
+            unlink($connection['database']);
+        });
+    }
+}
